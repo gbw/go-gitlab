6d63332b5704b5dff060e88c058215738b4abae7
Heidi Berry <heidi.berry@coop.co.uk>
2025-02-28 20:33:08 +0000

    Add support for instance member roles API, and align `CreateMemberRoleOptions` field names to JSON tags
    Changelog: Breaking Changes

--- member_roles_test.go
+++ member_roles_test.go
@@ -4,21 +4,22 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestListMemberRoles(t *testing.T) {
+func TestListInstanceMemberRoles(t *testing.T) {
 	mux, client := setup(t)
 
-	path := "/api/v4/groups/1/member_roles"
+	path := "/api/v4/member_roles"
 
 	mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, http.MethodGet)
 		mustWriteHTTPResponse(t, w, "testdata/list_member_roles.json")
 	})
 
-	memberRoles, _, err := client.MemberRolesService.ListMemberRoles(1)
-	require.NoError(t, err)
+	memberRoles, resp, err := client.MemberRolesService.ListInstanceMemberRoles()
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
 
 	want := []*MemberRole{
 		{
@@ -39,20 +40,20 @@ func TestListMemberRoles(t *testing.T) {
 		},
 	}
 
-	require.Equal(t, want, memberRoles)
+	assert.Equal(t, want, memberRoles)
 }
 
-func TestCreateMemberRole(t *testing.T) {
+func TestCreateInstanceMemberRole(t *testing.T) {
 	mux, client := setup(t)
 
-	path := "/api/v4/groups/84/member_roles"
+	path := "/api/v4/member_roles"
 
 	mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, http.MethodPost)
 		mustWriteHTTPResponse(t, w, "testdata/create_member_role.json")
 	})
 
-	memberRole, _, err := client.MemberRolesService.CreateMemberRole(84, &CreateMemberRoleOptions{
+	memberRole, resp, err := client.MemberRolesService.CreateInstanceMemberRole(&CreateMemberRoleOptions{
 		Name:                       Ptr("Custom guest"),
 		BaseAccessLevel:            Ptr(GuestPermissions),
 		Description:                Ptr("a sample custom role"),
@@ -66,9 +67,9 @@ func TestCreateMemberRole(t *testing.T) {
 		AdminWebHook:               Ptr(false),
 		ArchiveProject:             Ptr(false),
 		ManageDeployTokens:         Ptr(false),
-		ManageGroupAccesToken:      Ptr(false),
+		ManageGroupAccessTokens:    Ptr(false),
 		ManageMergeRequestSettings: Ptr(false),
-		ManageProjectAccessToken:   Ptr(false),
+		ManageProjectAccessTokens:  Ptr(false),
 		ManageSecurityPolicyLink:   Ptr(false),
 		ReadCode:                   Ptr(true),
 		ReadRunners:                Ptr(false),
@@ -77,7 +78,8 @@ func TestCreateMemberRole(t *testing.T) {
 		RemoveGroup:                Ptr(false),
 		RemoveProject:              Ptr(false),
 	})
-	require.NoError(t, err)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
 
 	want := &MemberRole{
 		ID:                         3,
@@ -95,9 +97,9 @@ func TestCreateMemberRole(t *testing.T) {
 		AdminWebHook:               false,
 		ArchiveProject:             false,
 		ManageDeployTokens:         false,
-		ManageGroupAccesToken:      false,
+		ManageGroupAccessTokens:    false,
 		ManageMergeRequestSettings: false,
-		ManageProjectAccessToken:   false,
+		ManageProjectAccessTokens:  false,
 		ManageSecurityPolicyLink:   false,
 		ReadCode:                   true,
 		ReadRunners:                false,
@@ -107,7 +109,126 @@ func TestCreateMemberRole(t *testing.T) {
 		RemoveProject:              false,
 	}
 
-	require.Equal(t, want, memberRole)
+	assert.Equal(t, want, memberRole)
+}
+
+func TestDeleteInstanceMemberRole(t *testing.T) {
+	mux, client := setup(t)
+
+	path := "/api/v4/member_roles/2"
+
+	mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodDelete)
+	})
+
+	resp, err := client.MemberRolesService.DeleteInstanceMemberRole(2)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestListMemberRoles(t *testing.T) {
+	mux, client := setup(t)
+
+	path := "/api/v4/groups/1/member_roles"
+
+	mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		mustWriteHTTPResponse(t, w, "testdata/list_member_roles.json")
+	})
+
+	memberRoles, resp, err := client.MemberRolesService.ListMemberRoles(1)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+
+	want := []*MemberRole{
+		{
+			ID:              1,
+			Name:            "GuestCodeReader",
+			Description:     "A Guest user that can read code",
+			GroupID:         1,
+			BaseAccessLevel: 10, // Guest Base Level
+			ReadCode:        true,
+		},
+		{
+			ID:                2,
+			Name:              "GuestVulnerabilityReader",
+			Description:       "A Guest user that can read vulnerabilities",
+			GroupID:           1,
+			BaseAccessLevel:   10, // Guest Base Level
+			ReadVulnerability: true,
+		},
+	}
+
+	assert.Equal(t, want, memberRoles)
+}
+
+func TestCreateMemberRole(t *testing.T) {
+	mux, client := setup(t)
+
+	path := "/api/v4/groups/84/member_roles"
+
+	mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		mustWriteHTTPResponse(t, w, "testdata/create_member_role.json")
+	})
+
+	memberRole, resp, err := client.MemberRolesService.CreateMemberRole(84, &CreateMemberRoleOptions{
+		Name:                       Ptr("Custom guest"),
+		BaseAccessLevel:            Ptr(GuestPermissions),
+		Description:                Ptr("a sample custom role"),
+		AdminCICDVariables:         Ptr(false),
+		AdminComplianceFramework:   Ptr(false),
+		AdminGroupMembers:          Ptr(false),
+		AdminMergeRequest:          Ptr(false),
+		AdminPushRules:             Ptr(false),
+		AdminTerraformState:        Ptr(false),
+		AdminVulnerability:         Ptr(false),
+		AdminWebHook:               Ptr(false),
+		ArchiveProject:             Ptr(false),
+		ManageDeployTokens:         Ptr(false),
+		ManageGroupAccessTokens:    Ptr(false),
+		ManageMergeRequestSettings: Ptr(false),
+		ManageProjectAccessTokens:  Ptr(false),
+		ManageSecurityPolicyLink:   Ptr(false),
+		ReadCode:                   Ptr(true),
+		ReadRunners:                Ptr(false),
+		ReadDependency:             Ptr(false),
+		ReadVulnerability:          Ptr(false),
+		RemoveGroup:                Ptr(false),
+		RemoveProject:              Ptr(false),
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+
+	want := &MemberRole{
+		ID:                         3,
+		Name:                       "Custom guest",
+		Description:                "a sample custom role",
+		BaseAccessLevel:            GuestPermissions,
+		GroupID:                    84,
+		AdminCICDVariables:         false,
+		AdminComplianceFramework:   false,
+		AdminGroupMembers:          false,
+		AdminMergeRequests:         false,
+		AdminPushRules:             false,
+		AdminTerraformState:        false,
+		AdminVulnerability:         false,
+		AdminWebHook:               false,
+		ArchiveProject:             false,
+		ManageDeployTokens:         false,
+		ManageGroupAccessTokens:    false,
+		ManageMergeRequestSettings: false,
+		ManageProjectAccessTokens:  false,
+		ManageSecurityPolicyLink:   false,
+		ReadCode:                   true,
+		ReadRunners:                false,
+		ReadDependency:             false,
+		ReadVulnerability:          false,
+		RemoveGroup:                false,
+		RemoveProject:              false,
+	}
+
+	assert.Equal(t, want, memberRole)
 }
 
 func TestDeleteMemberRole(t *testing.T) {
@@ -119,6 +240,7 @@ func TestDeleteMemberRole(t *testing.T) {
 		testMethod(t, r, http.MethodDelete)
 	})
 
-	_, err := client.MemberRolesService.DeleteMemberRole(1, 2)
-	require.NoError(t, err)
+	resp, err := client.MemberRolesService.DeleteMemberRole(1, 2)
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
 }
