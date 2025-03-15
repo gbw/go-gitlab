8ecad42574aa5255918b62448694cf8c6fe91528
Gary Holtz <gholtz@gitlab.com>
2025-03-15 09:20:13 -0500

    bug: Add fallback for macOS version of readlink
    Changelog: Improvements

--- scripts/generate_mock_api.sh
+++ scripts/generate_mock_api.sh
@@ -2,14 +2,24 @@
 
 # This script generates the `api_generated.go` file, which includes one `go:generate` line for each interface.
 # This is used to ensure we have a mocked setup for each interface we create in the client.
-testing_pkg_dir=$(readlink -e "$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)/../testing")
+
+case $(uname -s) in
+Darwin)
+  READLINK_FLAG="-f"
+  ;;
+*)
+  READLINK_FLAG="-e"
+  ;;
+esac
+
+testing_pkg_dir=$(readlink $READLINK_FLAG "$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)/../testing")
 api_file="$testing_pkg_dir/api_generated.go"
 
 (
   echo '// This file is generate from scripts/generate_mock_api.sh'
   echo 'package testing'
   echo ''
-) > "$api_file"
+) >"$api_file"
 
 # shellcheck disable=SC2162,SC2038
 grep -E '^\s[A-Z][a-zA-Z0-9]+Interface interface {$' -- *.go | awk '{ print $1 $2 }' | while read line; do
@@ -17,7 +27,7 @@ grep -E '^\s[A-Z][a-zA-Z0-9]+Interface interface {$' -- *.go | awk '{ print $1 $
   filename=${filename%.go}
   interface=$(echo "$line" | cut -d: -f2)
 
-  echo "//go:generate go run go.uber.org/mock/mockgen@v0.5.0 -typed -destination=${filename}_mock.go -package=testing gitlab.com/gitlab-org/api/client-go ${interface}" >> "$api_file"
+  echo "//go:generate go run go.uber.org/mock/mockgen@v0.5.0 -typed -destination=${filename}_mock.go -package=testing gitlab.com/gitlab-org/api/client-go ${interface}" >>"$api_file"
 done
 
 go fmt "$api_file"
--- scripts/generate_service_interface_map.sh
+++ scripts/generate_service_interface_map.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env sh
 
 # This script generates a map keyed from the Service structs to the interfaces they implement. This is used
-# to test that each of the service structs implement their interface properly, and automatically keeps the 
+# to test that each of the service structs implement their interface properly, and automatically keeps the
 # map up to date so that contributors who implement new services don't need to do as much manual work.
-root_dir=$(readlink -e "$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)/..")
+case $(uname -s) in
+Darwin)
+  READLINK_FLAG="-f"
+  ;;
+*)
+  READLINK_FLAG="-e"
+  ;;
+esac
+
+root_dir=$(readlink $READLINK_FLAG "$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)/..")
 api_service_map_test_file="$root_dir/gitlab_service_map_generated_test.go"
 
 (
@@ -12,7 +21,7 @@ api_service_map_test_file="$root_dir/gitlab_service_map_generated_test.go"
   echo ''
   echo 'var ('
   echo '  serviceMap = map[any]any{'
-) > "$api_service_map_test_file"
+) >"$api_service_map_test_file"
 
 # shellcheck disable=SC2162,SC2038
 grep -E '^\s[A-Z][a-zA-Z0-9]+Service struct {' -- *.go | awk '{ print $1 $2 }' | while read line; do
@@ -20,12 +29,12 @@ grep -E '^\s[A-Z][a-zA-Z0-9]+Service struct {' -- *.go | awk '{ print $1 $2 }' |
   filename=${filename%.go}
   service=$(echo "$line" | cut -d: -f2)
 
-  echo "&${service}{}: (*${service}Interface)(nil)," >> "$api_service_map_test_file"
+  echo "&${service}{}: (*${service}Interface)(nil)," >>"$api_service_map_test_file"
 done
 
 (
   echo '  }'
   echo ')'
-) >> "$api_service_map_test_file"
+) >>"$api_service_map_test_file"
 
 go fmt "$api_service_map_test_file"
