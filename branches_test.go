35bfe497d0f0896c6d3c309fcb2ae0b5bac98735
Tim Kuzmin <sinnerpocht@gmail.com>
2025-06-03 15:34:01 +0400

    test: branch test improvements
    Changelog: Improvements

--- branches_test.go
+++ branches_test.go
@@ -79,29 +79,54 @@ func TestBranchesService_ListBranches(t *testing.T) {
 
 	authoredDate := time.Date(2012, 6, 27, 5, 51, 39, 0, time.UTC)
 	committedDate := time.Date(2012, 6, 28, 3, 44, 20, 0, time.UTC)
-	want := []*Branch{{
-		Name:               DefaultBranch,
-		Merged:             false,
-		Protected:          true,
-		Default:            true,
-		DevelopersCanPush:  false,
-		DevelopersCanMerge: false,
-		CanPush:            true,
-		WebURL:             "https://gitlab.example.com/my-group/my-project/-/tree/master",
-		Commit: &Commit{
-			AuthorEmail:    "john@example.com",
-			AuthorName:     exampleEventUserName,
-			AuthoredDate:   &authoredDate,
-			CommittedDate:  &committedDate,
-			CommitterEmail: "john@example.com",
-			CommitterName:  exampleEventUserName,
-			ID:             "7b5c3cc8be40ee161ae89a06bba6229da1032a0c",
-			ShortID:        "7b5c3cc",
-			Title:          "add projects API",
-			Message:        "add projects API",
-			ParentIDs:      []string{"4ad91d3c1144c406e50c7b33bae684bd6837faf8"},
+	want := []*Branch{
+		{
+			Name:               DefaultBranch,
+			Merged:             false,
+			Protected:          true,
+			Default:            true,
+			DevelopersCanPush:  false,
+			DevelopersCanMerge: false,
+			CanPush:            true,
+			WebURL:             "https://gitlab.example.com/my-group/my-project/-/tree/master",
+			Commit: &Commit{
+				AuthorEmail:    "john@example.com",
+				AuthorName:     exampleEventUserName,
+				AuthoredDate:   &authoredDate,
+				CommittedDate:  &committedDate,
+				CommitterEmail: "john@example.com",
+				CommitterName:  exampleEventUserName,
+				ID:             "7b5c3cc8be40ee161ae89a06bba6229da1032a0c",
+				ShortID:        "7b5c3cc",
+				Title:          "add projects API",
+				Message:        "add projects API",
+				ParentIDs:      []string{"4ad91d3c1144c406e50c7b33bae684bd6837faf8"},
+			},
 		},
-	}}
+		{
+			Name:               "develop",
+			Merged:             false,
+			Protected:          true,
+			Default:            true,
+			DevelopersCanPush:  false,
+			DevelopersCanMerge: false,
+			CanPush:            true,
+			WebURL:             "https://gitlab.example.com/my-group/my-project/-/tree/develop",
+			Commit: &Commit{
+				AuthorEmail:    "john@example.com",
+				AuthorName:     exampleEventUserName,
+				AuthoredDate:   &authoredDate,
+				CommittedDate:  &committedDate,
+				CommitterEmail: "john@example.com",
+				CommitterName:  exampleEventUserName,
+				ID:             "7b5c3cc8be40ee161ae89a06bba6229da1032a0c",
+				ShortID:        "7b5c3cc",
+				Title:          "add projects API",
+				Message:        "add projects API",
+				ParentIDs:      []string{"4ad91d3c1144c406e50c7b33bae684bd6837faf8"},
+			},
+		},
+	}
 
 	b, resp, err := client.Branches.ListBranches(5, nil)
 	assert.NoError(t, err)
@@ -143,34 +168,58 @@ func TestBranchesService_ListBranchesWithSearch(t *testing.T) {
 	require.NotNil(t, resp)
 	require.NotNil(t, b)
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
-	assert.Len(t, b, 1, "Expected exactly one branch in the response")
 
 	authoredDate := time.Date(2012, 6, 27, 5, 51, 39, 0, time.UTC)
 	committedDate := time.Date(2012, 6, 28, 3, 44, 20, 0, time.UTC)
-	expectedBranch := &Branch{
-		Name:               DefaultBranch,
-		Merged:             false,
-		Protected:          true,
-		Default:            true,
-		DevelopersCanPush:  false,
-		DevelopersCanMerge: false,
-		CanPush:            true,
-		WebURL:             "https://gitlab.example.com/my-group/my-project/-/tree/master",
-		Commit: &Commit{
-			AuthorEmail:    "john@example.com",
-			AuthorName:     exampleEventUserName,
-			AuthoredDate:   &authoredDate,
-			CommittedDate:  &committedDate,
-			CommitterEmail: "john@example.com",
-			CommitterName:  exampleEventUserName,
-			ID:             "7b5c3cc8be40ee161ae89a06bba6229da1032a0c",
-			ShortID:        "7b5c3cc",
-			Title:          "add projects API",
-			Message:        "add projects API",
-			ParentIDs:      []string{"4ad91d3c1144c406e50c7b33bae684bd6837faf8"},
+	expectedBranches := []*Branch{
+		{
+			Name:               DefaultBranch,
+			Merged:             false,
+			Protected:          true,
+			Default:            true,
+			DevelopersCanPush:  false,
+			DevelopersCanMerge: false,
+			CanPush:            true,
+			WebURL:             "https://gitlab.example.com/my-group/my-project/-/tree/master",
+			Commit: &Commit{
+				AuthorEmail:    "john@example.com",
+				AuthorName:     exampleEventUserName,
+				AuthoredDate:   &authoredDate,
+				CommittedDate:  &committedDate,
+				CommitterEmail: "john@example.com",
+				CommitterName:  exampleEventUserName,
+				ID:             "7b5c3cc8be40ee161ae89a06bba6229da1032a0c",
+				ShortID:        "7b5c3cc",
+				Title:          "add projects API",
+				Message:        "add projects API",
+				ParentIDs:      []string{"4ad91d3c1144c406e50c7b33bae684bd6837faf8"},
+			},
+		},
+		{
+			Name:               "develop",
+			Merged:             false,
+			Protected:          true,
+			Default:            true,
+			DevelopersCanPush:  false,
+			DevelopersCanMerge: false,
+			CanPush:            true,
+			WebURL:             "https://gitlab.example.com/my-group/my-project/-/tree/develop",
+			Commit: &Commit{
+				AuthorEmail:    "john@example.com",
+				AuthorName:     exampleEventUserName,
+				AuthoredDate:   &authoredDate,
+				CommittedDate:  &committedDate,
+				CommitterEmail: "john@example.com",
+				CommitterName:  exampleEventUserName,
+				ID:             "7b5c3cc8be40ee161ae89a06bba6229da1032a0c",
+				ShortID:        "7b5c3cc",
+				Title:          "add projects API",
+				Message:        "add projects API",
+				ParentIDs:      []string{"4ad91d3c1144c406e50c7b33bae684bd6837faf8"},
+			},
 		},
 	}
-	assert.Equal(t, expectedBranch, b[0])
+	assert.Equal(t, expectedBranches, b)
 }
 
 func TestBranchesService_ListBranchesWithRegex(t *testing.T) {
@@ -192,34 +241,58 @@ func TestBranchesService_ListBranchesWithRegex(t *testing.T) {
 	require.NotNil(t, resp)
 	require.NotNil(t, b)
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
-	assert.Len(t, b, 1, "Expected exactly one branch in the response")
 
 	authoredDate := time.Date(2012, 6, 27, 5, 51, 39, 0, time.UTC)
 	committedDate := time.Date(2012, 6, 28, 3, 44, 20, 0, time.UTC)
-	expectedBranch := &Branch{
-		Name:               DefaultBranch,
-		Merged:             false,
-		Protected:          true,
-		Default:            true,
-		DevelopersCanPush:  false,
-		DevelopersCanMerge: false,
-		CanPush:            true,
-		WebURL:             "https://gitlab.example.com/my-group/my-project/-/tree/master",
-		Commit: &Commit{
-			AuthorEmail:    "john@example.com",
-			AuthorName:     exampleEventUserName,
-			AuthoredDate:   &authoredDate,
-			CommittedDate:  &committedDate,
-			CommitterEmail: "john@example.com",
-			CommitterName:  exampleEventUserName,
-			ID:             "7b5c3cc8be40ee161ae89a06bba6229da1032a0c",
-			ShortID:        "7b5c3cc",
-			Title:          "add projects API",
-			Message:        "add projects API",
-			ParentIDs:      []string{"4ad91d3c1144c406e50c7b33bae684bd6837faf8"},
+	expectedBranch := []*Branch{
+		{
+			Name:               DefaultBranch,
+			Merged:             false,
+			Protected:          true,
+			Default:            true,
+			DevelopersCanPush:  false,
+			DevelopersCanMerge: false,
+			CanPush:            true,
+			WebURL:             "https://gitlab.example.com/my-group/my-project/-/tree/master",
+			Commit: &Commit{
+				AuthorEmail:    "john@example.com",
+				AuthorName:     exampleEventUserName,
+				AuthoredDate:   &authoredDate,
+				CommittedDate:  &committedDate,
+				CommitterEmail: "john@example.com",
+				CommitterName:  exampleEventUserName,
+				ID:             "7b5c3cc8be40ee161ae89a06bba6229da1032a0c",
+				ShortID:        "7b5c3cc",
+				Title:          "add projects API",
+				Message:        "add projects API",
+				ParentIDs:      []string{"4ad91d3c1144c406e50c7b33bae684bd6837faf8"},
+			},
+		},
+		{
+			Name:               "develop",
+			Merged:             false,
+			Protected:          true,
+			Default:            true,
+			DevelopersCanPush:  false,
+			DevelopersCanMerge: false,
+			CanPush:            true,
+			WebURL:             "https://gitlab.example.com/my-group/my-project/-/tree/develop",
+			Commit: &Commit{
+				AuthorEmail:    "john@example.com",
+				AuthorName:     exampleEventUserName,
+				AuthoredDate:   &authoredDate,
+				CommittedDate:  &committedDate,
+				CommitterEmail: "john@example.com",
+				CommitterName:  exampleEventUserName,
+				ID:             "7b5c3cc8be40ee161ae89a06bba6229da1032a0c",
+				ShortID:        "7b5c3cc",
+				Title:          "add projects API",
+				Message:        "add projects API",
+				ParentIDs:      []string{"4ad91d3c1144c406e50c7b33bae684bd6837faf8"},
+			},
 		},
 	}
-	assert.Equal(t, expectedBranch, b[0])
+	assert.Equal(t, expectedBranch, b)
 }
 
 func TestBranchesService_CreateBranch(t *testing.T) {
